dcf8fff914bc7f640685c43bf43d2a4a7b10a6c3
Andreas Johansson <andreas@stdin.se>
2015-04-19 18:21:19 +0200

    Added support for adapters

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-var builder = module.exports = {};
+var _ = require('lodash');
 
-builder.html = require('./lib/html')(builder);
-builder.utils = require('./lib/utils')(builder);
-builder.inputs = require('./lib/inputs')(builder);
-builder.components = require('./lib/components')(builder);
+var builder = module.exports = {};
 
 builder.messages = {
     label: {},
     hint: {}
 };
+
+builder.reset = function() {
+    builder.html = require('./lib/html')(builder);
+    builder.utils = require('./lib/utils')(builder);
+    builder.inputs = require('./lib/inputs')(builder);
+    builder.components = require('./lib/components')(builder);
+    builder.fields = require('./lib/fields')(builder);
+}
+
+builder.reset();
+
+builder.use = function(definition) {
+    _.map(definition, function(adapter, type) {
+        require('./lib/' + type + '-adapters/' + adapter)(builder);
+    });
+}
